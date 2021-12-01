c8715dbf0fd725bf9d3bc61a476c5a8ed2fc067b
woongstaa <jinung91@gmail.com>
2021-12-01 15:19:33 +0900

    Add: first commit. 초기 세팅 완료.

---
+++ src/Router.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//효상님 components
+
+//민호님 components
+
+//태준님 components
+
+//종현님 components
+
+//진웅님 components
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* //효상님 route */}
+        <Route />
+        <Route />
+        <Route />
+        {/* //민호님 route */}
+        <Route />
+        <Route />
+        <Route />
+        {/* //태준님 route */}
+        <Route />
+        <Route />
+        <Route />
+        {/* //종현님 route */}
+        <Route />
+        <Route />
+        <Route />
+        {/* //진웅님 route */}
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
