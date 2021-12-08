38edfe45cf5173badb2e318957e2cb7e45dd0750
TaeJoonPark <tjpark1028@gmail.com>
2021-12-08 23:13:48 +0900

    Edit: code review 내용 50% 반영

--- src/pages/taejoon/Detail/Detail.js
+++ src/pages/taejoon/Detail/Detail.js
@@ -1,40 +1,37 @@
 import "./Detail.scss";
 import TopNav from "../components/TopNav";
+import React, {useEffect, useState} from 'react'
+
 
 function Detail() {
-
+  
+  
     return (
-        <>
-            <TopNav />
-            <div className="coffee_category">
-                <div className="coldBrew">
-                    콜드 브루
-                </div>
-                <div className="route">
-                    {'홈 > MENU > 음료 > 에스프레소 > 화이트 초콜릿 모카'}
-                </div>
+    <div className="containerDetail">
+      <TopNav />
+      <div className="coffee_category">
+        <div className="coldBrew">콜드 브루</div>
+        <div className="route">
+          {"홈 > MENU > 음료 > 에스프레소 > 화이트 초콜릿 모카"}
+        </div>
+      </div>
+      <div className="imgAndInfo">
+        <div className="coffee_img">
+          <img src="/images/details.jpeg" alt="coffee_pic" />
+        </div>
+        <div className="coffee_info">
+          <div className="nameAndLike">
+            <div className="coffee_name">
+              <h2>화이트 초콜릿 모카</h2>
+              <p>white Chocolate Mocha</p>
             </div>
-            <div className="imgAndInfo">
-                <div className="coffee_img">
-                    <img src="/images/coffee1.png" alt="" />
-                </div>
-                <div className="coffee_info">
-                    <div className="nameAndLike">
-                        <div className="coffee_name">
-                            <h2>화이트 초콜릿 모카</h2>
-                            white Chocolate Mocha
-                        </div>
-                        <div className="heart_icon">
-                            하트뿅
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="footer">
-                울랄라울랄라
-            </div>
-        </>
-    );
+            <div className="heart_icon">하트뿅</div>
+          </div>
+        </div>
+      </div>
+      <div className="footer">울랄라울랄라</div>
+    </div>
+  );
 }
 
 export default Detail;
